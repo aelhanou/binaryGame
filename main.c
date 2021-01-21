bd376ad9cbc5347a8ae0d8c50139cb937124eaf8
aelhanou <elhanouniazeddine00@gmail.com>
2021-01-21 10:58:44 +0100

    fonction x

---
+++ main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+
+// function has two int as an argument and do the addition and convert them to binary and return int
+
+
+
+
+
+
+
+
+int	main()
+{
+	
+	int a[3] = {1,1,0};
+	int b[3] = {0,1,1}
+	
+	
+	
+	
+	
+	
+	return 0;
+}
