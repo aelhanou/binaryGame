32c97c38af8c2de8314c68eaa2c15d0179d33188
achouqfi <75440007+achouqfi@users.noreply.github.com>
2021-01-21 12:28:02 +0100

    fist w

--- main.c
+++ main.c
@@ -4,7 +4,14 @@
 // function has two int as an argument and do the addition and convert them to binary and return int
 
 
-
+int DtoB(){
+	int a,
+		x;
+	int T[4];
+	printf("saisir un nombre decimal a converti : \n");
+	scanf("%d",&a);
+	
+}
 
 
 
@@ -14,7 +21,7 @@ int	main()
 {
 	
 	int a[3] = {1,1,0};
-	int b[3] = {0,1,1}
+	int b[3] = {0,1,1};
 	
 	
 	
